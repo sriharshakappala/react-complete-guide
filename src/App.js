4bff7f071a88c6456886bea8ecf611a0b63b442a
Sri Harsha Kappals <sriharsha.kappala@hotmail.com>
2020-05-06 21:16:00 +0530

    Children props demo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, this is react app</h1>
         <Person name='Max' age='28' />
-        <Person name='Mini' age='26' />
+        <Person name='Mini' age='26'>My hobbies are : Gaming</Person>
+        <Person name='Stephanie' age='24' />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
