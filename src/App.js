9092f867591d68180890998b48f59c67aa19aef9
Sri Harsha Kappals <sriharsha.kappala@hotmail.com>
2020-05-06 18:26:20 +0530

    React.createElement 4

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, this is react app</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    return (
+      <div className="App">
+        <h1>Hi, this is react app</h1>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
