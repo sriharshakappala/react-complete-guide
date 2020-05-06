82c52b40fdaf4c8517cd0c7752d1a3d4e64522d2
Sri Harsha Kappals <sriharsha.kappala@hotmail.com>
2020-05-06 18:22:54 +0530

    React.createElement 1

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, this is react app</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, this is react app</h1>
+    //   </div>
+    // );
+    return React.createElement('div', null, 'h1', 'This is sample react app');
   }
 }
 
