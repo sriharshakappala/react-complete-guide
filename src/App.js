771e7e625965ebcc65ae31f72b2f2952450a7803
Sri Harsha Kappals <sriharsha.kappala@hotmail.com>
2020-05-06 22:11:49 +0530

    Add click event handler

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: '28' },
+      { name: 'Mini', age: '26' },
+      { name: 'Stephanie', age: '24' }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, this is react app</h1>
-        <Person name='Max' age='28' />
-        <Person name='Mini' age='26'>My hobbies are : Gaming</Person>
-        <Person name='Stephanie' age='24' />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are : Gaming</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
