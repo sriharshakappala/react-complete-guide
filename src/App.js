fa0ea50871f8f2bf49c1460693a65c7c321d645a
Sri Harsha Kappals <sriharsha.kappala@hotmail.com>
2020-05-06 18:49:51 +0530

    Fully functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, this is react app</h1>
+        <Person />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
