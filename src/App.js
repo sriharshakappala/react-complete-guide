039c608a2a831924cf8a518e5016d1a55a8538de
Sri Harsha Kappals <sriharsha.kappala@hotmail.com>
2020-05-09 15:41:57 +0530

    Use radium for media queries

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import UserOutput from './UserOutput/UserOutput';
 import Validation from './Validation/Validation';
 import Char from './Char/Char';
 
-import Radium from 'radium'
+import Radium, {StyleRoot} from 'radium'
 
 class App extends Component {
   state = {
@@ -143,25 +143,27 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, this is react app</h1>
-        <p className={classes.join(' ')}>Classes Dynamic Demo</p>
-        {/* <button style={style} onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button> */}
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        { persons }
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, this is react app</h1>
+          <p className={classes.join(' ')}>Classes Dynamic Demo</p>
+          {/* <button style={style} onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button> */}
+          <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          { persons }
 
-        <UserInput changed={this.userNameChangedHandler} currentName={this.state.username} />
-        <UserOutput userName={this.state.username}/>
-        <UserOutput userName='Mini'/>
-        <UserOutput userName='Stephanie'/>
+          <UserInput changed={this.userNameChangedHandler} currentName={this.state.username} />
+          <UserOutput userName={this.state.username}/>
+          <UserOutput userName='Mini'/>
+          <UserOutput userName='Stephanie'/>
 
-        {/* Assignment 2 */}
-        <h2>Assignment 2</h2>
-        <input type='text' onChange={this.inputChangedHandler} value={this.state.userInput} />
-        <p>{this.state.userInput}</p>
-        <Validation inputLength={this.state.userInput.length} />
-        {charList}
-      </div>
+          {/* Assignment 2 */}
+          <h2>Assignment 2</h2>
+          <input type='text' onChange={this.inputChangedHandler} value={this.state.userInput} />
+          <p>{this.state.userInput}</p>
+          <Validation inputLength={this.state.userInput.length} />
+          {charList}
+        </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
